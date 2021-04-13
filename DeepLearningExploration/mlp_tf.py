0bab56e3ffc0c2ce2c727a661552a39dcc51ef0a
Gustavo Moreira <gustavo1995.2012@hotmail.com>
2021-04-13 20:00:43 +0100

    Implemented simple network that sums 2 numbers with tensorflow

---
+++ DeepLearningExploration/mlp_tf.py
@@ -0,0 +1,42 @@
+from random import random
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+
+# inputs = ([[0.1, 0.2], [0.3, 0.4]])
+# output = ([[0.3], [0.7]])
+
+def generate_dataset(num_samples, test_size):
+
+    x = np.array([[random() / 2 for _ in range(2)] for _ in range(num_samples)]) # array([[0.1, 0.2] [0.3, 0.4]])
+    y = np.array([[i[0] + i[1]] for i in x]) # array([[0.3] [0.7]])
+    x_train, x_test, y_train, t_test = train_test_split(x, y, test_size=test_size)
+    return x_train, x_test, y_train, t_test
+
+if __name__ == "__main__":
+    x_train, x_test, y_train, y_test = generate_dataset(5000, 0.3)
+
+
+    # build model: 2 -> 5 -> 1
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(5, input_dim=2, activation="sigmoid"),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+    # compile model
+    optimizer = tf.keras.optimizers.SGD(learning_rate=0.1)
+    model.compile(optimizer=optimizer, loss="MSE")
+
+    # train model
+    model.fit(x_train, y_train, epochs=100)
+
+    # evaluate model
+    print("\nModel evaluation")
+    model.evaluate(x_test, y_test, verbose=1)
+
+    # make predictions
+    data = np.array([[0.1, 0.2], [0.3, 0.4]])
+    predictions = model.predict(data)
+    print("\nSome predictions:")
+    for d, p in zip(data, predictions):
+        print("{} + {} = {}".format(d[0], d[1], p[0]))
